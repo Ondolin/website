c6c5d800394fcf832cc0dca8fe1ad6b18000af0f
Ondolin <developer@ondolin.de>
2021-11-18 19:01:30 +0100

    changed website name and favicon

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'Ondolin',
+  titleTemplate: "%s",
   plugins: [
     {
       use: "gridsome-plugin-tailwindcss",
