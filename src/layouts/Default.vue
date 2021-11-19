cf090f77c1e7cc00a2ca662f2a29d64538b5f9d8
Ondolin <developer@ondolin.de>
2021-11-19 23:17:15 +0100

    added background color to face color into safari overlay

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -26,7 +26,7 @@ export default {
 <style lang="scss">
 body {
   font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  @apply text-white leading-relaxed;
+  @apply text-white leading-relaxed bg-primary;
   //background-image: linear-gradient(to right bottom, #473e75, #4a407a, #4d437f, #504584, #534889, #564b8e, #5a4d92, #5d5097, #61549c, #6558a2, #6a5da7, #6e61ad);
   background-image: linear-gradient(to right bottom, #574b90, #5d4d98, #634fa0, #6951a8, #7053b0);
 }
